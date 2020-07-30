6a4b894a2eba6cb51b34b8787781b7bd366d5f7b
nicolas varone <varonenico@gmail.com>
2020-07-30 09:49:39 +0200

    Header, landing and footer

---
+++ src/components/App/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../Header";
+import Landing from "../Landing";
+import Footer from "../Footer";
+import "../../App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Landing />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-container">
+        <p>Projet réalisé par Nicolas Varone</p>
+        <p>
+          Les icônes Wolverine, Iron Man, Spider-Man et Batman sont tirées de
+          iconfinder.com
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
