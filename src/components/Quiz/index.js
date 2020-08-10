ad25a2b60c960a7c472561e2949eea74eb9461c0
nicolas varone <varonenico@gmail.com>
2020-08-10 15:50:16 +0200

    Questions answers

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,20 +1,63 @@
 import React, { Component } from "react";
+import { QuizMarvel } from "../QuizMarvel";
 import Levels from "../Levels";
 import ProgressBar from "../ProgressBar";
 
 class Quiz extends Component {
+  state = {
+    levelNames: ["debutant", "confirme", "expert"],
+    quizlevel: 0,
+    maxQuestions: 10,
+    storedQuestions: [],
+    questions: null,
+    options: [],
+    idQuestion: 0,
+  };
+
+  loadQuestions = (quizz) => {
+    const fetchedArrayQuiz = QuizMarvel[0].quizz[quizz];
+    if (fetchedArrayQuiz.length >= this.state.maxQuestions) {
+      const newArray = fetchedArrayQuiz.map(
+        ({ answer, ...keepRest }) => keepRest
+      );
+      this.setState({
+        storedQuestions: newArray,
+      });
+    } else {
+      console.log("Pas assez de questions");
+    }
+  };
+
+  componentDidMount() {
+    this.loadQuestions(this.state.levelNames[this.state.quizlevel]);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.storedQuestions !== prevState.storedQuestions) {
+      this.setState({
+        question: this.state.storedQuestions[this.state.idQuestion].question,
+        options: this.state.storedQuestions[this.state.idQuestion].options,
+      });
+    }
+  }
+
   render() {
     const { pseudo } = this.props.userData;
+
+    const displayOptions = this.state.options.map((option, index) => {
+      return (
+        <p key="index" className="answerOptions">
+          {option}
+        </p>
+      );
+    });
 
     return (
       <div>
         <Levels />
         <ProgressBar />
-        <h2>Notre question Quiz</h2>
-        <p className="answerOptions">Question 1</p>
-        <p className="answerOptions">Question 2</p>
-        <p className="answerOptions">Question 3</p>
-        <p className="answerOptions">Question 4</p>
+        <h2>{this.state.question}</h2>
+        {displayOptions}
         <button className="btnSubmit">Suivant</button>
       </div>
     );
