d8130cca0b684781fc46bc985103ada90ec58829
nicolas varone <varonenico@gmail.com>
2020-08-18 14:02:34 +0200

    React tooltip

--- src/components/Logout/index.js
+++ src/components/Logout/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
 import { FirebaseContext } from "../Firebase";
+import ReactTooltip from "react-tooltip";
 
 const Logout = () => {
   const firebase = useContext(FirebaseContext);
@@ -21,8 +22,9 @@ const Logout = () => {
     <div className="logoutContainer">
       <label className="switch">
         <input onChange={handleChange} type="checkbox" checked={checked} />
-        <span className="slider round"></span>
+        <span className="slider round" data-tip="Déconnexion"></span>
       </label>
+      <ReactTooltip place="left" effect="solid" />
     </div>
   );
 };
